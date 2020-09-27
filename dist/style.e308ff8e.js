0e79dc07d10f6aa74b94bdbabf44f75f71f15f9e
Danilson Bornachera <papuh662@gmail.com>
2020-09-26 21:49:23 -0500

    update

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -117,62 +117,79 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"objetos.js":[function(require,module,exports) {
-var mama = {
-  Nombre: 'Sandra',
-  Apellido: 'balaguera',
-  Edad: 47,
-  Sexo: 'Femenino'
-};
-var papá = {
-  Nombre: 'Wuilman',
-  Apellido: 'bornachera',
-  Edad: 52,
-  Sexo: 'Masculino'
-};
-var Hermana = {
-  Nombre: 'Zulay',
-  Apellido: 'bornachera',
-  Edad: 7,
-  Sexo: 'Femenino'
-};
-var Hermano = {
-  Nombre: 'Beiman',
-  Apellido: 'bornachera',
-  Edad: 14,
-  Sexo: 'Masculino'
-};
-var Hermano2 = {
-  Nombre: 'Anderson',
-  Apellido: 'bornachera',
-  Edad: 18,
-  Sexo: 'Masculino'
-};
-var MiPersona = {
-  Nombre: 'Danilson',
-  Apellido: 'bornachera',
-  Edad: 16,
-  Sexo: 'Masculino'
-};
-var MiGata = {
-  Nombre: 'Michi',
-  Apellido: '',
-  Edad: 1,
-  Sexo: 'Femenino'
-};
-var MiGato = {
-  Nombre: 'Raul',
-  Apellido: '',
-  Edad: '7 meses',
-  Sexo: 'Masculino'
-};
-var MyFamily = [MiGato, mama, papá, Hermana, Hermano, Hermano2, MiPersona, MiGata];
-console.log(MyFamily);
+})({"../../AppData/Roaming/nvm/v14.6.0/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
 
-for (var contador = 0; contador < MyFamily.length; contador++) {
-  console.log(MyFamily[contador]);
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
+  }
+
+  return bundleURL;
 }
-},{}],"../../AppData/Roaming/nvm/v14.6.0/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"../../AppData/Roaming/nvm/v14.6.0/node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"../../AppData/Roaming/nvm/v14.6.0/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"style.css":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"_css_loader":"../../AppData/Roaming/nvm/v14.6.0/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../AppData/Roaming/nvm/v14.6.0/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -200,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51985" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50097" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -376,5 +393,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../AppData/Roaming/nvm/v14.6.0/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","objetos.js"], null)
-//# sourceMappingURL=/objetos.d3b26e0e.js.map
+},{}]},{},["../../AppData/Roaming/nvm/v14.6.0/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+//# sourceMappingURL=/style.e308ff8e.js.map
